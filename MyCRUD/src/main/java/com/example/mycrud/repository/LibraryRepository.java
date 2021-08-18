093aaa7d8f92a8acb69316394e09b8c3ab8b2fb1
kimulchan <leerabi577@gmail.com>
2021-08-18 20:11:42 +0900

    ⚡:: library와 book repository 추가

--- MyCRUD/src/main/java/com/example/mycrud/repository/LibraryRepository.java
+++ MyCRUD/src/main/java/com/example/mycrud/repository/LibraryRepository.java
@@ -1,7 +1,6 @@
 package com.example.mycrud.repository;
 
-import com.example.mycrud.model.Book;
-import com.example.mycrud.model.Library;
+import com.example.mycrud.domain.model.Library;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LibraryRepository extends JpaRepository<Library,Integer> {
