debb27fb1167cd143c085e46b9f1cb91c91abb71
kimulchan <leerabi577@gmail.com>
2021-08-19 23:02:09 +0900

    ⚡:: Get controller 추가

--- MyCRUD/src/main/java/com/example/mycrud/controller/BookController.java
+++ MyCRUD/src/main/java/com/example/mycrud/controller/BookController.java
@@ -2,23 +2,31 @@ package com.example.mycrud.controller;
 
 import com.example.mycrud.domain.model.Book;
 import com.example.mycrud.dto.BookDto;
-import com.example.mycrud.service.BookRegistration;
-import com.example.mycrud.service.LibraryRegistration;
+import com.example.mycrud.dto.BookResponseDto;
+import com.example.mycrud.service.BookService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 public class BookController {
 
-    private final BookRegistration bookRegistration;
+    private final BookService bookService;
 
     @PostMapping("book")
     private String gogo(@RequestBody BookDto bookDto){
-        return bookRegistration.bookReg(bookDto);
+        return bookService.bookReg(bookDto);
+    }
+
+    @GetMapping("book")
+    public List<Book> getBook(){
+        return bookService.bookFind();
+    }
+
+    @GetMapping("book/{id}")
+    public List<Book> getBookId(@PathVariable Integer id){
+        return bookService.bookIdFind(id);
     }
 }
